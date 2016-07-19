dd1e2fe44033780446f2d5d57cc168a3f96b7f6a
hediant <hediant@163.com>
2016-07-19 19:12:56 +0800

    modified:   services/athena.info/info.js

--- services/athena.info/info.js
+++ services/athena.info/info.js
@@ -41,7 +41,7 @@ Information.prototype.init = function() {
 
 	this.pool_ = mysql.createPool(this.config_.server);
 	this.dbhelper_ = new DbHelper(this.pool_);
-	this.system_cache_ = new SystemCache(this.dbhelper_);
+	this.system_cache_ = new SystemCache(this.dbhelper_, this.config_.fetch_interval);
 
 	// Do NOT forget
 	this.emit('ready');
